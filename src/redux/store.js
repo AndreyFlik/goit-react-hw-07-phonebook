36fe96f29a8b178dc471d9fd2c3b76b8ecdfa23f
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-12-23 15:33:52 +0200

    Закомментировал персист

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,23 @@
 import { createLogger } from "redux-logger";
 import { configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+// import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import contactsReducer from "../redux/contacts/contacts-reducer";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
 
-const persistContactsConfig = {
-  key: "items",
-  storage,
-  whitelist: ["items"],
-};
+// const persistContactsConfig = {
+//   key: "items",
+//   storage,
+//   whitelist: ["items"],
+// };
 
 const logger = createLogger({
   collapsed: (getState, action, logEntry) => !logEntry.error,
@@ -26,17 +26,22 @@ const logger = createLogger({
 
 const store = configureStore({
   reducer: {
-    contacts: persistReducer(persistContactsConfig, contactsReducer),
+    // contacts: persistReducer(persistContactsConfig, contactsReducer),
+    contacts: contactsReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(logger),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== "production",
 });
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }).concat(logger),
+//   devTools: process.env.NODE_ENV !== "production",
+// });
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
-export { store, persistor };
+export { store };
+// export { store, persistor };
